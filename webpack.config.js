9441b1b9212c56793b6ac5e6703db7cec665c6ba
umar-muneer <umar.muneer@educative.io>
2018-10-01 18:19:24 +0500

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,33 @@
 const path = require('path');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-core',
+              options: {
+                presets: ['es2015']
+              }
+            }
+          ]
+        }, 
+        {
+          test: /\.scss$/,
+          use:
+            new ExtractPlugin({
+              filename: 'main.css'
+            }).extract({
+              use: ['scss-loader', 'css-loader']
+            })
+        }
+      ]
     }
 }
